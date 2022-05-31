cb18d16b752919aa349c5517fd8e107c32404beb
bjornjorgensen <bjornjorgensen@gmail.com>
2022-05-31 22:00:38 +0200

    more upgrades

--- resources/tools/azure-data-studio.sh
+++ resources/tools/azure-data-studio.sh
@@ -15,7 +15,7 @@ done
 if [ ! -f "/usr/share/azuredatastudio/azuredatastudio" ]; then
     echo "Installing Azure Data Studio. Please wait..."
     cd $RESOURCES_PATH
-    wget https://go.microsoft.com/fwlink/?linkid=2092022 -O ./azure-data-studio.deb
+    wget https://go.microsoft.com/fwlink/?linkid=2193237 -O ./azure-data-studio.deb
     apt-get update
     apt-get install -y ./azure-data-studio.deb
     rm ./azure-data-studio.deb
